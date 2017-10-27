e78d6566b65cd28adebceaa9043f751d79f408bf
DESKTOP-MVIIFK4\SergBelom <SergBelom@yandex.ru>
2017-10-27 17:54:40 +0300

    Add methods for read data from text.txt and Console

--- HuffmanDeconing/HuffmanDeconing/Program.cs
+++ HuffmanDeconing/HuffmanDeconing/Program.cs
@@ -1,20 +1,22 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Collections.Generic;
 
 namespace HuffmanDeconing
 {
     class Program
     {
-
-
-
         static void Main(string[] args)
         {
+            //string sLine = Console.ReadLine();
+
+            AlgoHuffmanDecoding.ReadLineFromFile();
+
+            AlgoHuffmanDecoding.ReadLineFromConsole();
+
+            Console.ReadKey();
         }
-
-
     }
 }

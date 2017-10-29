4dea672e18b5e15b935903ed6769a80e034edbc4
DESKTOP-MVIIFK4\SergBelom <SergBelom@yandex.ru>
2017-10-29 16:08:34 +0300

    Small changes

--- PriorityQueue/PriorityQueue/Program.cs
+++ PriorityQueue/PriorityQueue/Program.cs
@@ -1,15 +1,19 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Collections.Generic;
 
 namespace PriorityQueue
 {
     class Program
     {
+
+
+
         static void Main(string[] args)
         {
+
         }
     }
 }

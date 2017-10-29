164bcdc2a61e7c6c7fd03678fdb08cb0a5268a59
DESKTOP-MVIIFK4\SergBelom <SergBelom@yandex.ru>
2017-10-29 18:33:49 +0300

    Work Version, but no pass test 6 in STepik platform. Error Time Limit.

--- PriorityQueue/PriorityQueue/Program.cs
+++ PriorityQueue/PriorityQueue/Program.cs
@@ -8,11 +8,42 @@ namespace PriorityQueue
 {
     class Program
     {
-
-
-
         static void Main(string[] args)
         {
+            PriorityQueueGeneric<int> inputQueue = new PriorityQueueGeneric<int>();
+            List<int> result = new List<int>();
+
+            // считываем количество входных строк
+            int countLine;
+            countLine = int.Parse(Console.ReadLine());
+
+            // определяем переменные для хранения промежуточных элементов
+            string[] lineFromConsole;
+            int interValue = 0;
+            int resultInterValue = 0;
+
+            for (int i = 0; i < countLine; i++)
+            {
+                lineFromConsole = Console.ReadLine().Split(' ');
+
+                if (lineFromConsole[0] != "ExtractMax")
+                {
+                    interValue = int.Parse(lineFromConsole[1]);
+                    inputQueue.Add(int.Parse(lineFromConsole[1]), interValue);
+                }
+                else
+                {
+                    inputQueue.Poll(out resultInterValue, out interValue);
+                    result.Add(resultInterValue);
+                }
+            }
+
+            foreach (int elem in result)
+            {
+                Console.WriteLine(elem);
+            }
+
+            Console.ReadKey();
 
         }
     }
---
+++ PriorityQueue/PriorityQueue/ReadConsole.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+
+namespace PriorityQueue
+{
+    class DataFromConsole
+    {
+        public static Dictionary<string, int> ReadDataFromConsole()
+        {
+            Dictionary<string, int> InputData = new Dictionary<string, int>();
+
+            int countLine;
+            countLine = int.Parse(Console.ReadLine());
+
+            string[] lineFromConsole;
+            int interValue = 0;
+            for (int i = 0; i < countLine; i++)
+            {
+                lineFromConsole = Console.ReadLine().Split(' ');
+
+                if (lineFromConsole[0] != "ExtractMax")
+                {
+                    interValue = int.Parse(lineFromConsole[1]);
+                }
+
+                InputData.Add(lineFromConsole[0], interValue);
+            }
+
+            return InputData;
+        }
+    }
+}

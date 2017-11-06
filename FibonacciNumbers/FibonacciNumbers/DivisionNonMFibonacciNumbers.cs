58561a911c5a7645f475a98b089597369def6c99
DESKTOP-MVIIFK4\SergBelom <SergBelom@yandex.ru>
2017-11-07 01:55:36 +0300

    Small changes.

--- FibonacciNumbers/FibonacciNumbers/DivisionNonMFibonacciNumbers.cs
+++ FibonacciNumbers/FibonacciNumbers/DivisionNonMFibonacciNumbers.cs
@@ -8,13 +8,17 @@ namespace FibonacciNumbers
 {
     /*огромное число Фибоначчи по модулю
         Даны целые числа 1<n<10^18 и 2<m<10^5, необходимо найти остаток от деления n-го числа Фибоначчи на m.
+
+        Sample Input:
+        10 2
+        Sample Output:
+        1
    */
-   
+
     static class DivisionNonMFibonacciNumbers
     {
         public static void GenerateDivisionNonMFibonacciNumbers()
         {
-
 
         }
     }

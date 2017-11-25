8fd7c29ecce67108e6a21eefb7b1235442290c78
DESKTOP-MVIIFK4\SergBelom <SergBelom@yandex.ru>
2017-11-25 18:08:08 +0300

    Add read form console counts of segments and points.

--- Points/Points/Program.cs
+++ Points/Points/Program.cs
@@ -1,15 +1,51 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Collections.Generic;
+
+/* РЕЗУЛЬТАТ:
+ *  Для каждой точки в порядке появления во вводе выведите,
+ *  скольким отрезкам она принадлежит
+ */
 
 namespace Points
 {
     class Program
     {
+
+        private static int[] countsSegmentsAndPoints = { 0, 0 };
+       
+        // считать отрезки -> 2мерный массив координат отрезков
+        static int[,] CountSegmants( )
+        {
+
+            return null;
+        }
+        
+        // считать все точки -> 1мерный массив координат точек
+        static int[] CountPoints( )
+        {
+
+            return null;
+        }
+
+        // подсчет и вывод отрезков, в которые входит данная точка
+        static void CountSegmantsForSelectedPoint(  )
+        {
+
+        }
+
+        // алгоритм быстрой сортировки
+
+        
         static void Main(string[] args)
         {
+            // считываем с консоли два числа n - кол-во отрезков и m - кол-во точек на прямой и изменяем поле countsSegmentsAndPoints
+            countsSegmentsAndPoints = Array.ConvertAll(Console.ReadLine().Split(' ') , int.Parse);
+            
+
+
         }
     }
 }

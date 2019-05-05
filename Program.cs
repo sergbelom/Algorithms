f8d5f63b6539e1ab5e97d2930ed71d5c28361d2c
sergbelom <SergBelom@yandex.ru>
2019-05-05 07:43:14 +0300

    Add code.

---
+++ Program.cs
@@ -0,0 +1,67 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EditDistance
+{
+    class Program
+    {
+        public class EditDistance
+        {
+            public static int Calculate(string a, string b)
+            {
+                var d = new int[b.Length + 1, a.Length + 1];
+
+                for (int j = 0; j < a.Length + 1; j++)
+                {
+                    d[0, j] = j;
+                }
+                for (int i = 0; i < b.Length + 1; i++)
+                {
+                    d[i, 0] = i;
+                }
+
+                for (int i = 1; i < b.Length + 1; i++)
+                {
+                    for (int j = 1; j < a.Length + 1; j++)
+                    {
+                        d[i, j] = Min(
+                            d[i, j - 1] + 1,
+                            d[i - 1, j] + 1,
+                            d[i - 1, j - 1] + Diff(a[j - 1], b[i - 1])
+                        );
+                    }
+                }
+
+                return d[b.Length, a.Length];
+            }
+
+            private static int Min(int a, int b, int c)
+            {
+                return Math.Min(a, Math.Min(b, c));
+            }
+
+            private static int Diff(char a, char b)
+            {
+                if (a==b)                
+                    return 0;                
+                else                
+                    return 1;
+                
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            var first = Console.ReadLine();
+            var second = Console.ReadLine();
+
+            // расстояние редактирования двух данных непустых строк длины не более 102, содержащих строчные буквы латинского алфавита.
+            
+            Console.WriteLine(EditDistance.Calculate(first, second));
+            Console.ReadKey();
+        }
+    }   
+}
